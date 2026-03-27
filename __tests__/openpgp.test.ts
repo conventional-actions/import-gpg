c40ee7898ea79ee4fb93ddf6595547e459de13b9
Anton Winter <anton@ketch.com>
2026-03-26 21:41:32 -0700

    feat!: Migrate to ESM, esbuild, and Node 24
    BREAKING CHANGE: Minimum runner must support node24.

--- __tests__/openpgp.test.ts
+++ __tests__/openpgp.test.ts
@@ -1,6 +1,6 @@
-import {describe, expect, it} from '@jest/globals';
-import * as fs from 'fs';
-import * as openpgp from '../src/openpgp';
+import {describe, expect, it} from '@jest/globals'
+import * as fs from 'fs'
+import * as openpgp from '../src/openpgp'
 
 const userInfos = [
   {
@@ -41,9 +41,12 @@ const userInfos = [
     email: 'joe@bar.foo',
     keyID: '6071D218380FDCC8',
     fingerprint: '87F257B89CE462100BEC0FFE6071D218380FDCC8',
-    keygrips: ['F5C3ABFAAB36B427FD98C4EDD0387E08EA1E8092', 'DEE0FC98F441519CA5DE5D79773CB29009695FEB']
+    keygrips: [
+      'F5C3ABFAAB36B427FD98C4EDD0387E08EA1E8092',
+      'DEE0FC98F441519CA5DE5D79773CB29009695FEB'
+    ]
   }
-];
+]
 
 for (const userInfo of userInfos) {
   // eslint-disable-next-line jest/valid-title
@@ -51,43 +54,45 @@ for (const userInfo of userInfos) {
     describe('readPrivateKey', () => {
       it('returns a PGP private key from an armored string', async () => {
         await openpgp.readPrivateKey(userInfo.pgp).then(privateKey => {
-          expect(privateKey.keyID).toEqual(userInfo.keyID);
-          expect(privateKey.name).toEqual(userInfo.name);
-          expect(privateKey.email).toEqual(userInfo.email);
-          expect(privateKey.fingerprint).toEqual(userInfo.fingerprint);
-        });
-      });
+          expect(privateKey.keyID).toEqual(userInfo.keyID)
+          expect(privateKey.name).toEqual(userInfo.name)
+          expect(privateKey.email).toEqual(userInfo.email)
+          expect(privateKey.fingerprint).toEqual(userInfo.fingerprint)
+        })
+      })
       it('returns a PGP private key from a base64 armored string', async () => {
         await openpgp.readPrivateKey(userInfo.pgp_base64).then(privateKey => {
-          expect(privateKey.keyID).toEqual(userInfo.keyID);
-          expect(privateKey.name).toEqual(userInfo.name);
-          expect(privateKey.email).toEqual(userInfo.email);
-          expect(privateKey.fingerprint).toEqual(userInfo.fingerprint);
-        });
-      });
-    });
+          expect(privateKey.keyID).toEqual(userInfo.keyID)
+          expect(privateKey.name).toEqual(userInfo.name)
+          expect(privateKey.email).toEqual(userInfo.email)
+          expect(privateKey.fingerprint).toEqual(userInfo.fingerprint)
+        })
+      })
+    })
 
     describe('generateKeyPair', () => {
       it('generates a PGP key pair', async () => {
-        await openpgp.generateKeyPair(userInfo.name, userInfo.email, userInfo.passphrase).then(keyPair => {
-          expect(keyPair).not.toBeUndefined();
-          expect(keyPair.publicKey).not.toBeUndefined();
-          expect(keyPair.privateKey).not.toBeUndefined();
-        });
-      }, 30000);
-    });
+        await openpgp
+          .generateKeyPair(userInfo.name, userInfo.email, userInfo.passphrase)
+          .then(keyPair => {
+            expect(keyPair).not.toBeUndefined()
+            expect(keyPair.publicKey).not.toBeUndefined()
+            expect(keyPair.privateKey).not.toBeUndefined()
+          })
+      }, 30000)
+    })
 
     describe('isArmored', () => {
       it('returns true for armored key string', async () => {
         await openpgp.isArmored(userInfo.pgp).then(armored => {
-          expect(armored).toEqual(true);
-        });
-      });
+          expect(armored).toEqual(true)
+        })
+      })
       it('returns false for base64 key string', async () => {
         await openpgp.isArmored(userInfo.pgp_base64).then(armored => {
-          expect(armored).toEqual(false);
-        });
-      });
-    });
-  });
+          expect(armored).toEqual(false)
+        })
+      })
+    })
+  })
 }
--- src/git.ts
+++ src/git.ts
@@ -1,4 +1,4 @@
-import * as exec from '@actions/exec';
+import * as exec from '@actions/exec'
 
 const git = async (args: string[] = []): Promise<string> => {
   return await exec
@@ -8,17 +8,21 @@ const git = async (args: string[] = []): Promise<string> => {
     })
     .then(res => {
       if (res.stderr.length > 0 && res.exitCode != 0) {
-        throw new Error(res.stderr);
+        throw new Error(res.stderr)
       }
-      return res.stdout.trim();
-    });
-};
+      return res.stdout.trim()
+    })
+}
 
-export async function setConfig(key: string, value: string, global: boolean): Promise<void> {
-  const args: Array<string> = ['config'];
+export async function setConfig(
+  key: string,
+  value: string,
+  global: boolean
+): Promise<void> {
+  const args: Array<string> = ['config']
   if (global) {
-    args.push('--global');
+    args.push('--global')
   }
-  args.push(key, value);
-  await git(args);
+  args.push(key, value)
+  await git(args)
 }
